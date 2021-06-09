3df7e8cb9db692e7972edff10193a424cd831db4
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2021-06-09 11:36:15 +0800

    歪比巴卜

---
+++ js/choice.js
@@ -0,0 +1,41 @@
+// 定时器
+let time = null;
+// 已选中的ID集合
+let selIds = [];
+$(function () {
+    let index = 0;
+    let lis = $(".left-content ul li");
+    time = setInterval(function () {
+        index++;
+        let random = Math.ceil(Math.random() * (lis.length + 1));
+        lis.removeClass("check");
+        let li = $(lis).eq(random - 1);
+        $(li).addClass("check");
+        if (li.attr('id') != null) {
+            if (index % 10 === 0) {
+                if (selIds.length === 0) {
+                    selIds.push(li.attr("id"));
+                    appendToRight(li.find("span").text());
+                } else {
+                    for (let i = 0; i < selIds.length; i++) {
+                        if ($.inArray(li.attr('id'), selIds) === -1) {
+                            selIds.push(li.attr("id"));
+                            appendToRight(li.find("span").text());
+                        }
+                    }
+                }
+            }
+        }
+        // 选中5个就停止
+        if (selIds.length === 5) {
+            clearInterval(time);
+            $(".left-content ul li").removeClass("check");
+        }
+    }, 100);
+})
+
+// 选中的项目添加到右侧列表
+function appendToRight(name) {
+    let html = "<li>" + name + "</li>";
+    $(".right-content .select-result").append(html);
+}
